e171792d1b4c5c54936857483627a5b6b2901a7d
caleb okogbe <okogbecaleb@gmail.com>
2020-03-25 08:40:08 +0100

    first input

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+function generateTarget(){
+    return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(humanGuess, computerGuess, targetGuess) {
+    computerGuess = generateTarget();
+  
+    humanGuess = Math.abs(targetGuess - humanGuess);
+    computerGuess = Math.abs(targetGuess - computerGuess);
+    
+    
+    if (computerGuess > humanGuess && computerGuess >= computerGuess || computerGuess < humanGuess && computerGuess <= computerGuess ) {
+      return true;
+    } else if (computerGuess < humanGuess) {
+      return false;
+    }
+  }
+  
+  
+  function updateScore(winner) {
+    if (winner == 'human') {
+      humanScore = humanScore + 1;
+    } else {
+      computerScore = computerScore + 1;
+    }
+  }
+  
+  function advanceRound() {
+    currentRoundNumber++
+  }
